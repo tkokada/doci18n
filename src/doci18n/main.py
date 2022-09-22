87589474cf77ccf0f9ab3c605a0dc990db3fcb05
Takashi OKADA <tk.okada.github@gmail.com>
2022-09-23 00:29:57 +0900

    initial

---
+++ src/doci18n/main.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def docret(input_string: str) -> str:
+    """
+    Just returns the input string.
+
+    Args:
+        input_string (str): An input string.
+
+    Returns:
+        str: Returns the input string.
+    """
+    return input_string
+
+
+def main() -> None:
+    """
+    Just prints the input string or default string.
+
+    Returns:
+        None: Nothing is returned.
+    """
+    input_string = sys.argv[1] if len(sys.argv) > 1 else "Hello, doci18n!"
+    print(docret(input_string))
